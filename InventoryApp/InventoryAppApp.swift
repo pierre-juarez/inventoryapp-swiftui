22df998c0b109896dd33abc8ab0e1fec194dc78b
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-01 01:04:29 -0500

    [add] - Initial commit 🚀

---
+++ InventoryApp/InventoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryAppApp.swift
+//  InventoryApp
+//
+//  Created by Pierre Juarez U. on 1/04/23.
+//
+
+import SwiftUI
+
+@main
+struct InventoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
